5b4075e1268dd3ed1c4fb442a2dc90c810c79a41
dishant1123 <dishantshah1417@gmail.com>
2025-10-29 12:00:17 +0530

    ....

--- 29oct_numpy.py
+++ 29oct_numpy.py
@@ -1,3 +1,4 @@
+import numpy as np 
 # task :2 
 """
 1 2 3 4 5 
@@ -15,7 +16,7 @@ print  only below  this element  :
 
 # task  :3 
 """
-1 2 3 4 5 
+1 2 3 4 5           # index ==> 0   ==> 1 2 3 4 5  ==>  index num 0 1 2 3 4 
 6 7 8 9 10 
 11 12 13 14 15 
 16 17 18 19 20 
@@ -26,6 +27,13 @@ print  only below  this element  :
 print  only below  this element  :
 
 output : [2,8,14,20]
+"""
+
+"""
+a= np.arange(1,31).reshape(6,5) 
+print(a)
+#        r   c 
+print(a[[0,1,2,3],[1,2,3,4]])
 """
 # task :4 
 """
@@ -44,3 +52,17 @@ ouptut  : [[4,5],
 """
 
 
+# a= np.arange(1,31).reshape(6,5) 
+# print(a)
+# print(a[0,3:])
+# print(a[4,3:])
+# print(a[5,3:])
+
+# print(a[[0,4,5],3:])
+
+
+c =np.random.randint(low=0,high=10,size=10)
+print(c)
+
+print(np.where(c>5))
+# print(d)
