ffa6480389bce60c63e8566fa6a071b9bf8c653f
dishant1123 <dishantshah1417@gmail.com>
2025-11-14 11:22:55 +0530

    ....

---
+++ 14nov.py
@@ -0,0 +1,7 @@
+"""
+task  :1  both csv 1 col delete.   ==> unamed 
+task  :2  join  ==> director ==>id  , movies ==> director_id 
+    display ==> 
+     director_id   ,director_name , title  
+
+"""
