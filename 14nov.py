febc4c288073bd13c7a5e2f1699d5ddf8e911e49
dishant1123 <dishantshah1417@gmail.com>
2025-11-14 11:40:22 +0530

    ....

--- 14nov.py
+++ 14nov.py
@@ -4,4 +4,11 @@ task  :2  join  ==> director ==>id  , movies ==> director_id
     display ==> 
      director_id   ,director_name , title  
 
+task :3  Get me the number of directors who did not produce any movie ever.
+
+task :4 Is there any director present in movies dataframe who is not in directors dataframe?
+
+task :5 select * from movies where vote_average > 7 .
+
+task :6 select title, director_name, revenue from movies where vote_average > 7  
 """
