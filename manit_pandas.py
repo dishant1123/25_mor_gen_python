277929f6a26b553f76755c1d7551e77768095575
dishant1123 <dishantshah1417@gmail.com>
2025-12-10 15:44:03 +0530

    ....

--- manit_pandas.py
+++ manit_pandas.py
@@ -7,4 +7,22 @@ df =pd.read_csv("mckinsey.csv")
 # print(df["year"])
 
 # print(df.head(20))
-print(df.tail(20))
+# print(df.tail(20))
+
+# print(df.info())
+
+# print(df[["country","population"]].head(20))
+
+# print(df.shape)
+
+# manually create a dataframe 
+
+a=pd.DataFrame([
+    ['a',23,70000],
+    ['b',13,7000],
+    ['c',43,700000],
+    ['d',53,7000000],
+    ['e',73,70000789]
+    ],columns=['name','age','salary']
+)
+print(a)
