2e7eb187fafc2400f5cba797f2ff66140984b268
dishant1123 <dishantshah1417@gmail.com>
2026-01-02 14:59:55 +0530

    ...

--- manit_matplot.py
+++ manit_matplot.py
@@ -27,9 +27,32 @@ plt.show()
 
 # pie chart :
 
-labels =["python","java","c++","c"]
+"""labels =["python","java","c++","c"]
 sizes =[30,20,10,5]
 
 plt.pie(sizes,labels=labels)
 plt.title("Programming languges Version")
+plt.show()
+"""
+
+# histogram :
+
+"""data =[10,20,20,28,35,40,40,40,50]
+
+plt.hist(data,bins=6)
+plt.title("Histogram")
+plt.xlabel("values")
+plt.ylabel("frequency")
+plt.show()
+"""
+
+# scatter plot :
+
+x=[1,2,3,4,5]
+y=[5,7,8,10,12] 
+
+plt.scatter(x,y)
+plt.xlabel("X axis")
+plt.ylabel("Y axis")
+plt.title("Scatter Plot")
 plt.show()
